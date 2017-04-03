b82a8c58a8a8c7c804b46ff8bc58b42590ee276e
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-04-03 01:20:40 -0500

    checking in latest derivation code and pdf

--- sym_deriv.py
+++ sym_deriv.py
@@ -1,51 +1,100 @@
+from contextlib import contextmanager
 from sympy import *
+from pprint import pprint
 import re
-t = symbols('t')
+
+t, r_1, r_2, l_1, l_2, g, m_1, I_1, m_2, I_2, f_1, f_2, b_1, b_2 = symbols('t r_1 r_2 l_1 l_2 g m_1 I_1 m_2 I_2 f_1 f_2 b_1 b_2')
+T1 = Function('T1')(t)
+T2 = Function('T2')(t)
+T1_dot = Derivative(T1, t)
+T2_dot = Derivative(T2, t)
+T1_ddot = Derivative(T1_dot, t)
+T2_ddot = Derivative(T2_dot, t)
+
+
 init_printing(use_unicode=True)
 
 theta_re = re.compile(r"\\operatorname{T_{(\d+)}}{\\left \(t \\right \)}")
 deriv_re = re.compile(r"\\frac{d}{d t}\s+\\operatorname{T_{(\d+)}}{\\left \(t \\right \)}")
 dderiv_re = re.compile(r"\\frac{d\^\{2}}{d t\^\{2}}\s+\\operatorname{T_{(\d+)}}{\\left \(t \\right \)}")
 
-def pformat(s):
-    s = dderiv_re.sub(r"\\ddot{\\theta_\g<1>}", s)
-    s = deriv_re.sub(r"\\dot{\\theta_\g<1>}", s)
-    s = theta_re.sub(r"\\theta_\g<1>", s)
-    return "=& " + s + r"\\"
 
-G = 9.8  # acceleration due to gravity, in m/s^2
-L1 = 1.0  # length of pendulum 1 in m
-M1 = 1.0  # mass of pendulum 1 in kg
-I1 = (M1 * L1 * L1) / 3  # moment of inertia of pendulum 1, fixed at end
-L2 = 1.0  # length of pendulum 2 in m
-M2 = 1.0  # mass of pendulum 2 in kg
-I2 = (M2 * L2 * L2) / 3  # moment of inertia of pendulum 2, fixed at end
-R1 = L1/2
-R2 = L2/2
+def pformat(equation, prefix=None):
+    if prefix is None:
+        prefix = ""
+    equation = latex(equation)
+    equation = dderiv_re.sub(r"\\ddot{\\theta_\g<1>}", equation)
+    equation = deriv_re.sub(r"\\dot{\\theta_\g<1>}", equation)
+    equation = theta_re.sub(r"\\theta_\g<1>", equation)
+    return prefix + "=& " + equation + r" & \\"
 
-T1 = "T1(t)"
-T2 = "T2(t)"
 
-x1 = "r_1*cos({0})".format(T1)
-y1 = "r_1*sin({0})".format(T1)
-x2 = "l_1*cos({0}) + r_2*cos({0}+{1})".format(T1,T2)
-y2 = "l_1*sin({0}) + r_2*sin({0}+{1})".format(T1,T2)
+@contextmanager
+def flalign_tag():
+    print(r"\begin{flalign}")
+    yield
+    print(r"\end{flalign}")
+    print(r"")
+
+
+def print_lagrange(index, lagrange, theta, theta_dot, time):
+
+    a = simplify(diff(lagrange, theta_dot))
+    b = simplify(diff(a, time))
+    c = simplify(diff(lagrange, theta))
+    d = simplify(b - c)
+
+    d_dot = r"\frac{{\partial{{L}}}}{{\partial{{\dot{{\theta_{index}}}}}}} {equation}"
+    d_dot_t = r"\frac{{d}}{{dt}}\left(\frac{{\partial{{L}}}}{{\partial{{\dot{{\theta_{index}}}}}}}\right) {equation}"
+    d_theta = r"\frac{{\partial{{L}}}}{{\partial{{\theta_{index}}}}} {equation}"
+    combined = r"\frac{{d}}{{dt}}\left(\frac{{\partial{{L}}}}{{\partial{{\dot{{\theta_{index}}}}}}}\right) - \frac{{\partial{{L}}}}{{\partial{{\theta_{index}}}}} {equation}"
+
+    print(d_dot.format(index=index, equation=pformat(a)))
+    print(d_dot_t.format(index=index, equation=pformat(b)))
+    print(d_theta.format(index=index, equation=pformat(c)))
+    print(combined.format(index=index, equation=pformat(d)))
+
+    return d
+
+x1 = r_1 * cos(T1)
+y1 = r_1 * sin(T1)
+x2 = l_1 * cos(T1) + r_2 * cos(T1 + T2)
+y2 = l_1 * sin(T1) + r_2 * sin(T1 + T2)
 
 x1_dot = diff(x1, t)
 y1_dot = diff(y1, t)
 x2_dot = diff(x2, t)
 y2_dot = diff(y2, t)
 
-T1_dot = diff(T1, t)
-T2_dot = diff(T2, t)
+K = ((m_1 * (x1_dot ** 2 + y1_dot ** 2) + m_2*(x2_dot ** 2 + y2_dot ** 2) + I_1 * T1_dot ** 2 + I_2 * T2_dot ** 2) / 2)
+U = m_1 * g * l_1 * sin(T1) + m_2 * g * (l_1 * sin(T1) + l_2 * sin(T1 + T2))
+L = K - U
 
-K = "(m_1*(({0})^2+({1})^2) + m_2*(({2})^2+({3})^2) + I_1*({4})^2 + I_2*({5})^2)/2".format(x1_dot, y1_dot, x2_dot, y2_dot, T1_dot, T2_dot)
-U = "m_1*g*l_1*sin({0})+m_2*g*(l_1*sin({0})+l_2*sin({0}+{1}))".format(T1, T2)
-L = "{0} - {1}".format(K, U)
+with flalign_tag():
+    print(pformat(simplify(x1), prefix="x_1"))
+    print(pformat(simplify(y1), prefix="y_1"))
+    print(pformat(simplify(x2), prefix="x_2"))
+    print(pformat(simplify(y2), prefix="y_2"))
+    print(pformat(simplify(x1_dot), prefix="\dot{x_1}"))
+    print(pformat(simplify(y1_dot), prefix="\dot{y_1}"))
+    print(pformat(simplify(x2_dot), prefix="\dot{x_2}"))
+    print(pformat(simplify(y2_dot), prefix="\dot{y_2}"))
 
+with flalign_tag():
+    print(r"K =& \tfrac{1}{2}I_1\omega_1^2 + \tfrac{1}{2}m_1v_1^2 + \tfrac{1}{2}I_2\omega_2^2 + \tfrac{1}{2}m_2v_2^2 & \\")
+    print(pformat(simplify(K)))
 
-print(pformat(latex(factor(L))))
-print(pformat(latex(factor(diff(L, T1)))))
-print(pformat(latex(factor(diff(L, T2)))))
-print(pformat(latex(factor(diff(diff(L, T1_dot),t)))))
-print(pformat(latex(collect(diff(diff(L, T2_dot),t), T2_dot))))
+with flalign_tag():
+    print(r"U =& m_1gl_1\sin(\theta_1) + m_2g(l_1\sin(\theta_1)+l_2\sin(\theta_1+\theta_2)) & \\")
+    print(pformat(simplify(U)))
+
+with flalign_tag():
+    print(r"L =& K -U & \\")
+    print(pformat(simplify(L)))
+
+with flalign_tag():
+    L1 = print_lagrange(1, L, T1, T1_dot, t)
+    L2 = print_lagrange(2, L, T2, T2_dot, t)
+
+Lagrange1 = L1 - (f_1 + b_1 * T1_dot)
+Lagrange2 = L2 - (f_2 + b_2 * T2_dot)
